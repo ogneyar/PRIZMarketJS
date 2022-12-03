635a77f4eebb5b109160065aa860e960f60812c0
ogneyar <ogneyar-ne@ya.ru>
2022-12-03 22:56:13 +0300

    minor edits

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 // import logo from '../../assets/logo.svg'
 import logo from '../../assets/prizmarket_logo.png'
